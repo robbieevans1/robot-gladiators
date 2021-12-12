e7378043caf14db4dc209ae7ade0101838c3aebe
robcabobb <robcabobb@gmail.com>
2021-12-11 23:29:51 -0500

    fixed shop() functoin to handle integer inputs

--- asset/js/game.js
+++ asset/js/game.js
@@ -168,29 +168,25 @@ var fight = function(enemy) {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', OR 'LEAVE' to make a choice."
-    );
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
+      );    
+    
+      // convert answer from prompt to an actual number
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
-            playerInfo.refillHealth();
-            break;
-        case "UPGRADE":
-        case "upgrade":
-            playerInfo.upgradeAttack();
-            break;
-        case "LEAVE": // new case
-        case "leave":
+        case 1:
+          playerInfo.refillHealth();
+          break;
+        case 2:
+          playerInfo.upgradeAttack();
+          break;
+        case 3:
           window.alert("Leaving the store.");
-          
-          // do nothing, so function will end
           break;
         default:
           window.alert("You did not pick a valid option. Try again.");
-          
-          // call shop() again to force player to pick a valid option
           shop();
           break;
       }
